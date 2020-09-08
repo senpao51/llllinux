5c5ac043576e624addb9ab900197d0e7fa701bf7
senpao51 <1773552201@qq.com>
2020-09-08 23:56:40 +0800

    web简易服务器

--- httpserver/testcgi.cpp
+++ httpserver/testcgi.cpp
@@ -4,7 +4,10 @@
 #include <unistd.h>
 #include "Util.h"
 using namespace std;
-
+void GetSubString(string str,int& x,int& y)
+{
+		
+}
 int main()
 {
 	char* content_length = getenv("Content-Length"); 
@@ -19,7 +22,17 @@ int main()
 			read(0,&c,1);
 			args+=c;
 		}
-		cout<<args<<endl;
+		cout<<"<html><h3>";
+		int x,y;
+		GetSubString(args,x,y);
+		cout<<"data1 + data2 = "<< x + y <<"<br/>"<<endl;
+		cout<<"data1 - data2 = "<< x - y <<"<br/>"<<endl;
+		cout<<"data1 * data2 = "<< x * y <<endl;
+		if(y!=0)
+			cout<<"data1 / data2 = "<<  x/y <<endl;
+		else
+			cout<<"div zero! error!"<<endl;
+		cout<<"</h3></html>"<<endl;
 	}
 	else
 	{
